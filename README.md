f729413089f9fc2bf8978d71e190a6dbbd94e2b2
Daniel Richter <danielrichter@posteo.de>
2023-03-17 22:20:28 +0100

    doc: update documentation
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # proxmox-cloudimages
-ready-to-clone cloud images for proxmox
+
+> heavily inspired by the following posts / projects
+> * https://austinsnerdythings.com/2023/01/10/proxmox-ubuntu-22-04-jammy-lts-cloud-image-script/
+> * https://github.com/TechByTheNerd/cloud-image-for-proxmox
+> 
+> However, since I will use these images mainly to create VMs via IaC solutions like pulumi or terraform my scripts are much simpler and do only the absolute bare minimum to create a template, leaving everything else like adding sshkeys, network config or even the network interface itself to the the IaC solution. 
+
+## Usage
+Run on of these commands on your workstation that can connect to your proxmox host at <user@host> over ssh, alternatively checkout `template.sh` and run it directly on the proxmox host
+```
+# Download image
+curl https://raw.githubusercontent.com/danielr1996/proxmox-cloudimages/main/template.sh | ssh <user@host> 'bash -'
+# use image "image.qcow2"
+curl https://raw.githubusercontent.com/danielr1996/proxmox-cloudimages/main/template.sh | ssh <user@host> 'bash /dev/stdin image.qcow2'
+```
+
+This script assumes a few things about your proxmox environment, namely: 
+* the ID of the template will be 9000
+* the lates ubuntu jammy image will be downloaded
+* the template will be stored on 'local-lvm'
+* the disk will be attached with 'virtio-scsi-pci'
+
+If you need to change any of these, download the script and edit the values on top
--- template.sh
+++ template.sh
@@ -7,29 +7,27 @@ storage_location='local-lvm'
 scsihw='virtio-scsi-pci'
 memory='512'
 cores='1'
-# bridge='vmbr0'
 
+# if no argument is set download the image from cloud_image_url
 if [ -z $1 ];then
     download_name='jammy.img'
     rm -rf $download_name
     wget ${cloud_image_url} -O ${download_name}
+# if an argument is set, use that as the location for a preloaded .img
 else
     download_name=$1
 fi
 
-# Cleanup
 qm destroy ${vm_id}
 
-# Prepare image
- virt-customize --update -a ${download_name}
- virt-customize --install qemu-guest-agent,cloud-init -a ${download_name} --truncate /etc/machine-id
+# the --truncate option is really important here, because it will reset /etc/machine-id to be empty, so it will be populated at each first boot 
+# of the cloned vm. Without it each cloned vm would inherit the machine-id from this template. This is neccessary because tools like k0s rely on 
+# this to uniquely identify a machine
+virt-customize --update -a ${download_name}
+virt-customize --install qemu-guest-agent,cloud-init -a ${download_name} --truncate /etc/machine-id
 
-# import image
-# --net0 virtio,bridge=${bridge}
- qm create ${vm_id} --memory ${memory} --cores ${cores}  --name ${template_name} --tags ubuntu,ubuntu-jammy,ubuntu22.04
- qm importdisk ${vm_id} ${download_name} ${storage_location}
- qm set ${vm_id} --scsihw ${scsihw} --scsi0 ${storage_location}:vm-${vm_id}-disk-1
-#  qm set ${vm_id} --ide0 ${storage_location}:cloudinit
- qm set ${vm_id} --boot c --bootdisk scsi0
-#  qm set ${vm_id} --agent enabled=1
- qm template ${vm_id}
+qm create ${vm_id} --memory ${memory} --cores ${cores}  --name ${template_name} --tags ubuntu,ubuntu-jammy,ubuntu22.04
+qm importdisk ${vm_id} ${download_name} ${storage_location}
+qm set ${vm_id} --scsihw ${scsihw} --scsi0 ${storage_location}:vm-${vm_id}-disk-1
+qm set ${vm_id} --boot c --bootdisk scsi0
+qm template ${vm_id}
